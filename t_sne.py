a0fe0b1bda52e215051d172a4eb8f7e8beb01f12
VP999274 <vijay.pandey@helloverify.com>
2020-04-02 17:07:52 +0530

    adding code

---
+++ t_sne.py
@@ -0,0 +1,64 @@
+from __future__ import print_function
+import time
+import numpy as np
+import pandas as pd
+from sklearn.datasets import fetch_mldata
+from sklearn.decomposition import PCA
+from sklearn.manifold import TSNE
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import seaborn as sns
+
+mnist = fetch_mldata("MNIST original")
+X = mnist.data / 255.0
+y = mnist.target
+print(X.shape, y.shape)
+
+feat_cols = [ 'pixel'+str(i) for i in range(X.shape[1]) ]
+df = pd.DataFrame(X,columns=feat_cols)
+df['y'] = y
+df['label'] = df['y'].apply(lambda i: str(i))
+X, y = None, None
+print('Size of the dataframe: {}'.format(df.shape))
+# [out] Size of the dataframe: (70000, 785)
+
+# For reproducability of the results
+np.random.seed(42)
+rndperm = np.random.permutation(df.shape[0])
+
+plt.gray()
+fig = plt.figure( figsize=(16,7) )
+for i in range(0,15):
+    ax = fig.add_subplot(3,5,i+1, title="Digit: {}".format(str(df.loc[rndperm[i],'label'])) )
+    ax.matshow(df.loc[rndperm[i],feat_cols].values.reshape((28,28)).astype(float))
+plt.show()
+
+pca = PCA(n_components=3)
+pca_result = pca.fit_transform(df[feat_cols].values)
+df['pca-one'] = pca_result[:,0]
+df['pca-two'] = pca_result[:,1] 
+df['pca-three'] = pca_result[:,2]
+print('Explained variation per principal component: {}'.format(pca.explained_variance_ratio_))
+# Explained variation per principal component: [0.09746116 0.07155445 0.06149531]
+plt.figure(figsize=(16,10))
+sns.scatterplot(
+    x="pca-one", y="pca-two",
+    hue="y",
+    palette=sns.color_palette("hls", 10),
+    data=df.loc[rndperm,:],
+    legend="full",
+    alpha=0.3
+)
+
+ax = plt.figure(figsize=(16,10)).gca(projection='3d')
+ax.scatter(
+    xs=df.loc[rndperm,:]["pca-one"], 
+    ys=df.loc[rndperm,:]["pca-two"], 
+    zs=df.loc[rndperm,:]["pca-three"], 
+    c=df.loc[rndperm,:]["y"], 
+    cmap='tab10'
+)
+ax.set_xlabel('pca-one')
+ax.set_ylabel('pca-two')
+ax.set_zlabel('pca-three')
+plt.show()
